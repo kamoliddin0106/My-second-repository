83c2f77773b7f995966962b3fc43babdcc8a7d5d
Hewlett-Packard <kamoliddinshukurov967@gmail.com>
2024-01-14 19:22:58 +0500

    My first commit in intellij

--- src/uz/pdp/lesson_9/homework/Student.java
+++ src/uz/pdp/lesson_9/homework/Student.java
@@ -3,5 +3,27 @@ package uz.pdp.lesson_9.homework;
 import java.io.Serializable;
 
 public class Student implements Serializable {
+    private String name;
+    private String age;
 
+    public Student(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
