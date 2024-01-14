e9de3f954effb8aade8ad330af1812c19a7ae078
Hewlett-Packard <kamoliddinshukurov967@gmail.com>
2024-01-14 19:18:05 +0500

    My first commit in intellij

---
+++ src/uz/pdp/lesson_9/homework/db/UserRepo.java
@@ -0,0 +1,28 @@
+package uz.pdp.lesson_9.homework.db;
+
+import uz.pdp.lesson_9.homework.User;
+
+import java.util.List;
+
+public class UserRepo implements Repository<User>{
+
+    @Override
+    public void save(User user) {
+
+    }
+
+    @Override
+    public void update(User user) {
+
+    }
+
+    @Override
+    public List<User> findAll(User user) {
+        return null;
+    }
+
+    @Override
+    public void delete(User user) {
+
+    }
+}
