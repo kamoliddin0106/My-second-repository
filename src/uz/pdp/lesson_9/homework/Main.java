0002c7ffa5afaacb4853c92366482cd2720e461a
Hewlett-Packard <kamoliddinshukurov967@gmail.com>
2024-01-15 18:25:25 +0500

    Student deleted

--- src/uz/pdp/lesson_9/homework/Main.java
+++ src/uz/pdp/lesson_9/homework/Main.java
@@ -1,7 +1,25 @@
 package uz.pdp.lesson_9.homework;
 
+import uz.pdp.lesson_8.classes.service.UserService;
+import uz.pdp.lesson_8.homework.h_2.util.Input;
+import uz.pdp.lesson_9.homework.db.StudentRepo;
+import uz.pdp.lesson_9.homework.service.StudentService;
+
 public class Main {
     public static void main(String[] args) {
+       while (true){
+           StudentService.printAll();
+           System.out.println("""
+                   1-Add student
+                   2-Delete student
+                   3-Update student
+                   """);
+           switch (Input.inputInt("Choose")){
+               case 1 -> StudentService.addStudent();
+               case 2 -> StudentService.deleteStudent();
+               case 3 -> StudentService.updateStudent();
+           }
+       }
 
     }
 }
---
+++ src/uz/pdp/lesson_9/homework/service/StudentService.java
@@ -0,0 +1,20 @@
+package uz.pdp.lesson_9.homework.service;
+
+public class StudentService {
+
+    public static void printAll() {
+
+    }
+
+    public static void addStudent() {
+
+    }
+
+    public static void deleteStudent() {
+
+    }
+
+    public static void updateStudent() {
+
+    }
+}
